e0ab9a4ae0978bb1b8abac0501a55809bb288e61
Андрей <@DESKTOP-77EI537>
2024-09-10 23:05:07 +0300

    Добавьте файлы проекта.

---
+++ Labb1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>	
+#include <locale.h>
+void main()	
+{
+	puts("     />___/>   ");
+	puts("    | _  _ |   ");
+	puts("   / =__*_=/   ");
+	puts("  /       |    ");
+	puts("  |   } } |    ");
+	puts("  |   | | |    ");
+	puts(" / /}__|__)_)  ");
+	puts(" |=|           ");
+	puts("  {_}          ");
+
+}
